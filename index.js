7f6450789dace1bf0ea5869ee56396f7c3a97d80
Rama Krushna K <45250889+RamaKrushna92@users.noreply.github.com>
2024-12-01 18:36:32 +0530

    Update index.js
    removed line no.8

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hello All, It's Sunday !!</h2>");
-  res.send("<h3>Welcome To The Party !!!</h3>");
+  res.send("<h2>Hello All, Welcome To The Party !!!</h2>");
 });
 
 const port = process.env.PORT || 3000;
