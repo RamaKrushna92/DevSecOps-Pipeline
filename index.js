c1533a312bb9afb450279504ff922809bce20b8b
Rama Krushna K <45250889+RamaKrushna92@users.noreply.github.com>
2024-12-01 14:44:34 +0530

    Update index.js
    msg update at line no. 6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hi There, Welcome to the Party. it's DevSecOps Pipeline</h2>");
+  res.send("<h2>Hi All, Welcome to the Party. Today Is Sunday !!</h2>");
 });
 
 const port = process.env.PORT || 3000;
