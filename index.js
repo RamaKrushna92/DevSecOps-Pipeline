b37fdcdf5fa2e37765c49b272faf1ac6420be830
Ubuntu <ubuntu@ip-172-31-94-206.ec2.internal>
2024-12-05 10:45:49 +0000

    updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h2>Welcome, This is DevSecOps Pipeline !!!</h2>");
+  res.send("<h2>Welcome, This is Application Security !!!</h2>");
 });
 
 const port = process.env.PORT || 3000;
